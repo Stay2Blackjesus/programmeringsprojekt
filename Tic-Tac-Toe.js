06e036f92dd8f7a48c7d0775ef0d50f162c26f6c
Stay2Blackjesus <71070994+Stay2Blackjesus@users.noreply.github.com>
2021-01-18 12:32:43 +0100

    Update Tic-Tac-Toe.js
    små ändringar

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -1,30 +1,48 @@
 //Changing this Value will trigger the start screen 0=OFF 1=ON
 var Menu = 0;
-var f = createFont("cursive");
+var f = createFont("e");
+//Variable för hur Start knappen ser ut
+    var sbtn = {
+    X: 120,
+    Y: 170,
+    Width: 160,
+    Height: 60
+    };
 
 if(Menu === 0){
     background(168, 165, 168);
-     //start Button
-     fill(0, 0, 0);
+    
+    //Shadow For Name of Game
+    fill(0, 0, 0);
+    textFont(f,38);
+    text("Tic-Tac-Toe",90.0,89);
+
+    //Startknappen
+    fill(0, 0, 0);
      strokeWeight(3);
      stroke(255, 0, 255);
-    ellipse(200,200,160,60);
+    rect(sbtn.X,sbtn.Y,sbtn.Width,sbtn.Height);
      //text for start Button
     fill(255, 255, 255);
     textSize(25);
     text("Start",174,207);
     
-    //Shadow For Name of Game
-    fill(0, 0, 0);
-    textFont(f,38);
-    text("Tic-Tac-Toe",98.6,89);
-    
     //Name of the game
     fill(255, 0, 0);
     textFont(f,38);
-    text("Tic-Tac-Toe",100,90);
+    text("Tic-Tac-Toe",92,91);
+    
+    //Name of the Creator
+    fill(0, 0, 0);
+    textFont(f,20);
+    text("By Kais.T",300,380);
 }
 
-//this line will show the cursor
-cursor();
-
+//button Core
+    mouseClicked = function() {
+    if (mouseX >= sbtn.X && mouseX <= sbtn.X+sbtn.Width &&
+        mouseY >= sbtn.Y && mouseY <= sbtn.Y+sbtn.Height)
+        {
+        Menu = 1; 
+        }
+};
